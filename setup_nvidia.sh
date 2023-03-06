a4e01537b1b3b6984886d401ec34e6cafccc1497
Devin Kodituwakku <74156492+ninetyninebytes@users.noreply.github.com>
2023-03-06 07:40:25 -0700

    Using DNF to download VS Code

--- setup_nvidia.sh
+++ setup_nvidia.sh
@@ -2,13 +2,15 @@
 
 dnf makecache
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf -y check-update
+dnf -y install code
 dnf -y update
 dnf -y install vim vlc htop gnome-tweaks gnome-extensions-app keepassxc neofetch gparted
 mkdir setup-extras
 cd setup-extras
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf -y install ./google*.rpm
-wget https://az764295.vo.msecnd.net/stable/92da9481c0904c6adfe372c12da3b7748d74bdcb/code-1.76.0-1677667581.el7.x86_64.rpm
-dnf -y install ./code*.rpm
 dnf -y install akmod-nvidia
 reboot
